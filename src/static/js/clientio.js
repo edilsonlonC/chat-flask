9722fb4e0da46212a3ed1ce836eac86fc1635321
StebanGi <54375250+StebanGi@users.noreply.github.com>
2020-06-15 11:02:43 -0500

    Salas a input de chat

--- src/static/js/clientio.js
+++ src/static/js/clientio.js
@@ -7,34 +7,67 @@ let roomjoin = $('#roomjoin')
 let textroom = $('#textroom')
 let listrooms= $('#listrooms')
 let linkrooms = $('button')
+let lastroom = ''
 
 console.log(linkrooms);
+console.log('holi');
 
 
 function render_message(data) {
     console.log(data);
     if(data.room){
-        boxchat.append(`<span> ${data.room} </span>`)
+        boxchat.append(`<span class="bg-info text-white font-weight-bold"> ${data.room} </span>`)
+    }   else{
+        lastroom = ''
     }
     if (data.nickname === nickname)
-        boxchat.append(`<span> me :  ${data.data} <span> <br>`)
+        boxchat.append(`<span class="text-white font-weight-bold"> yo:  ${data.data} <span> <br>`)
     else
        boxchat.append(`<span> ${data.nickname} :  ${data.data} <span> <br>`)
-
+    boxchat.scrollTop(boxchat[0].scrollHeight)
 }
 
 function render_rooms(rooms){
-
+    var vista_rooms = '<h2 class="mt-3 ml-3 text-white">' +
+                    'SALAS' +
+                '</h2>' +
+                '<div class="rooms-container-create text-center ml-3">' +
+                    '<!-- join and create room-->' +
+                    '<form class="form-inline" id="roomjoin" action="javascript:void(0)">' +
+                        '<div class="form-group">' +
+                            '<label for="textroom" class="sr-only">Nombre sala</label>' +
+                            '<input class="form-control col-sm" id="textroom" type="text" placeholder="Nombre sala">' +
+                        '</div>' +
+                        '<button class="btn btn-success" type="submit">Ingresar</button>' +
+                        '<!-- <input class="btn btn-success" type="submit" value="join room"> -->' +
+                    '</form>' +
+                '</div>';
+    var encabezado = '<br><div class="btn-group-vertical ml-3" role="group" aria-label="Basic example">';
+    var pie = '</div>';
     let string_to_rooms = ``
     for(var i = 0; i < rooms.length; i++){
-        string_to_rooms += `<button class="buttonsrooms"> ${rooms[i]} </button> <br>`
+        string_to_rooms += `<button class="btn btn-primary" onclick='room_autocomp("${rooms[i]}")'> ${rooms[i]} </button>`
     }
     console.log(string_to_rooms);
     
-    listrooms.html(string_to_rooms)
-
-
+    listrooms.html(vista_rooms+encabezado+string_to_rooms+pie)
 }
+
+function room_autocomp(room){
+    lastroom = '/' + room + ' '
+    message.val(lastroom)
+}
+
+function iniciar_sesion(){
+    $('#registro').hide();
+    $('#inicio_sesion').show();
+}
+
+function registrarse(){
+    $('#inicio_sesion').hide();
+    $('#registro').show();
+}
+
 function main()
 {
 
@@ -48,13 +81,13 @@ socket.on('connect', function () {
             data: message_to_send,
             nickname
         })
-        message.val('')
+        message.val(lastroom)
         
     })
 
-    linkrooms.click(function(){
+    /*linkrooms.click(function(){
         alert('click')
-    })
+    })*/
     socket.on('message', data => {
         console.log(data);
         render_message(data)
